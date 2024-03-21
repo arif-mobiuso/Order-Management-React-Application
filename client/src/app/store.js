ff4b1c4fbf104d8cc93cd0b0928e6cc548e88f33
Arif Shaik <arif.shaik@mobiuso.com>
2024-03-21 13:23:56 +0530

    Add redux-persistence

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,12 +1,28 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from '../features/cart/CartSlice';
 import userReducer from "../features/user/userSlice";
+import { persistReducer  , persistStore} from "redux-persist";
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from '@reduxjs/toolkit';
+
+
+const persistConfig = {
+  key: "root", 
+  storage, 
+  // whitelist : ['cart']
+}
+
+
+const rootReducer = combineReducers({
+  cart: cartReducer,
+  user: userReducer
+}) 
+
+
+const persistedReducer = persistReducer(persistConfig , rootReducer)
 
 export const store =  configureStore({
-  reducer: {
-    cart: cartReducer,
-    user: userReducer
-  },
+  reducer: persistedReducer,
 });
 
 
